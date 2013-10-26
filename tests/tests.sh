a5c2171ecdd0aceb25206ea683626d6f74b052d8
Pierre Pronchery <khorben@defora.org>
2013-10-26 19:43:03 +0200

    Fixed the copyright information

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 #$Id$
-#Copyright (c) 2012-2013 Pierre Pronchery <khorben@defora.org>
-#This file is part of DeforaOS System libc
+#Copyright (c) 2013 Pierre Pronchery <khorben@defora.org>
+#This file is part of DeforaOS Desktop Panel
 #This program is free software: you can redistribute it and/or modify
 #it under the terms of the GNU General Public License as published by
 #the Free Software Foundation, version 3 of the License.
