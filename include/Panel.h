d2d75cebd7733b06eaec5efcb27fcfd1c2284798
Pierre Pronchery <khorben@defora.org>
2013-11-21 02:16:05 +0100

    Documented the messaging protocol

--- include/Panel.h
+++ include/Panel.h
@@ -1,5 +1,5 @@
 /* $Id$ */
-/* Copyright (c) 2009-2012 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2009-2013 Pierre Pronchery <khorben@defora.org> */
 /* This file is part of DeforaOS Desktop Panel */
 /* This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -69,8 +69,8 @@ typedef struct _PanelAppletDefinition
 
 typedef enum _PanelMessage
 {
-	PANEL_MESSAGE_SHOW = 0,
-	PANEL_MESSAGE_EMBED
+	PANEL_MESSAGE_SHOW = 0,		/* PanelMessageShow, gboolean visible */
+	PANEL_MESSAGE_EMBED		/* Window */
 } PanelMessage;
 
 typedef enum _PanelMessageShow
