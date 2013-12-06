519b6337b981208adeae07a7daeec1af3c70391e
Pierre Pronchery <khorben@defora.org>
2013-12-06 08:43:34 +0100

    Fixed sorting by BSSID

--- tools/wifibrowser.c
+++ tools/wifibrowser.c
@@ -108,7 +108,7 @@ int main(int argc, char * argv[])
 	renderer = gtk_cell_renderer_text_new();
 	column = gtk_tree_view_column_new_with_attributes("BSSID", renderer,
 			"text", 0, NULL);
-	gtk_tree_view_column_set_sort_column_id(column, 3);
+	gtk_tree_view_column_set_sort_column_id(column, 1);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(view), column);
 	gtk_container_add(GTK_CONTAINER(widget), view);
 	gtk_container_add(GTK_CONTAINER(window), widget);
