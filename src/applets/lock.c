117de8d83585e28e67ee87764ae97e79ba5288a9
Pierre Pronchery <khorben@defora.org>
2018-01-17 00:45:17 +0100

    Remove the preferences interface
    It is no longer relevant, now that locking is handled outside of this
    applet.

--- src/applets/lock.c
+++ src/applets/lock.c
@@ -31,17 +31,12 @@ typedef struct _PanelApplet
 {
 	PanelAppletHelper * helper;
 	GtkWidget * widget;
-
-	/* preferences */
-	GtkWidget * pr_box;
-	GtkWidget * pr_command;
 } Lock;
 
 
 /* prototypes */
 static Lock * _lock_init(PanelAppletHelper * helper, GtkWidget ** widget);
 static void _lock_destroy(Lock * lock);
-static GtkWidget * _lock_settings(Lock * lock, gboolean apply, gboolean reset);
 
 /* callbacks */
 static void _lock_on_clicked(gpointer data);
@@ -56,7 +51,7 @@ PanelAppletDefinition applet =
 	NULL,
 	_lock_init,
 	_lock_destroy,
-	_lock_settings,
+	NULL,
 	FALSE,
 	TRUE
 };
@@ -77,8 +72,6 @@ static Lock * _lock_init(PanelAppletHelper * helper, GtkWidget ** widget)
 		return NULL;
 	}
 	lock->helper = helper;
-	lock->pr_box = NULL;
-	lock->pr_command = NULL;
 	lock->widget = gtk_button_new();
 	iconsize = panel_window_get_icon_size(helper->window);
 	image = gtk_image_new_from_icon_name("gnome-lockscreen", iconsize);
@@ -100,52 +93,6 @@ static void _lock_destroy(Lock * lock)
 {
 	gtk_widget_destroy(lock->widget);
 	free(lock);
-}
-
-
-/* lock_settings */
-static GtkWidget * _lock_settings(Lock * lock, gboolean apply, gboolean reset)
-{
-	GtkWidget * hbox;
-	GtkWidget * widget;
-	char const * p;
-
-#ifdef DEBUG
-	fprintf(stderr, "DEBUG: %s(%p, %s, %s)\n", __func__, (void *)lock,
-			apply ? "TRUE" : "FALSE", reset ? "TRUE" : "FALSE");
-#endif
-	if(lock->pr_box == NULL)
-	{
-#if GTK_CHECK_VERSION(3, 0, 0)
-		lock->pr_box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 4);
-		hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 4);
-#else
-		lock->pr_box = gtk_vbox_new(FALSE, 4);
-		hbox = gtk_hbox_new(FALSE, 4);
-#endif
-		widget = gtk_label_new(_("Command:"));
-		gtk_box_pack_start(GTK_BOX(hbox), widget, FALSE, TRUE, 0);
-		lock->pr_command = gtk_entry_new();
-		gtk_box_pack_start(GTK_BOX(hbox), lock->pr_command, TRUE, TRUE,
-				0);
-		gtk_box_pack_start(GTK_BOX(lock->pr_box), hbox, FALSE, TRUE, 0);
-		gtk_widget_show_all(lock->pr_box);
-		reset = TRUE;
-	}
-	if(reset == TRUE)
-	{
-		if((p = lock->helper->config_get(lock->helper->panel, "lock",
-						"command")) == NULL)
-			p = "xset s activate";
-		gtk_entry_set_text(GTK_ENTRY(lock->pr_command), p);
-	}
-	if(apply == TRUE)
-	{
-		p = gtk_entry_get_text(GTK_ENTRY(lock->pr_command));
-		lock->helper->config_set(lock->helper->panel, "lock", "command",
-				p);
-	}
-	return lock->pr_box;
 }
 
 
